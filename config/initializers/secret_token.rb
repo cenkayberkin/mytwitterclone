7a6dbebae293a953f3a5d65a55a7c4ad286897b1
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2014-06-11 22:51:24 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTwitterClone::Application.config.secret_token = 'df364bf34f1e55d690a01aee3bc9213da2b362d8d395a97859d3b42db12f2c0451b980a435b84e6b84c3b0e44a4cb6a42a3ea5f6b3210836cf1b00a1277313df'
