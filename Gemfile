e5cec8a90a76a89ab237cdf9f422584f4646fe3b
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2014-06-12 11:54:24 +0300

    Before upgrade to rails 3.2.17

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ end
 
 group :production do
   gem 'pg'
-  # gem 'rails_12factor'
+  gem 'rails_12factor'
 end
 
 # To use ActiveModel has_secure_password
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+describe "Static Pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
-
     end
+
   end
+
 end
