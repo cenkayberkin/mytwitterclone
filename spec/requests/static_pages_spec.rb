c598bd91ed3316346e57933e57e0e5c3267d6bf9
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-06-21 15:52:44 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,26 +7,38 @@ describe "Static Pages" do
   describe "Home page" do
     before { visit root_path }
     it { should have_selector('h1',text: 'My Twitter Clone App') } 
-    it { should have_selector('title', text: "MyTwitterClone") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text:  "| Home") }
   end
 
   describe "Help pagez" do
     before { visit help_path }
     it { should have_selector('h1',text:  'Help') }
-    it { should have_selector('title', text: "MyTwitterClone | Help") }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
     it { should have_selector('h1',text:  'About Us') }
-    it { should have_selector('title', text: "MyTwitterClone | About") }
+    it { should have_selector('title', text: full_title('About Us') ) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
     it { should have_selector('h1',text:  'Contact') }
-    it {  should have_selector('title', text:  "MyTwitterClone | Contact") }
+    it {  should have_selector('title', text:  full_title('Contact')) }
   end
-    
+  
+  it "should have the right links on the layout " do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title',text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title',text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title',text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title',text: full_title('Sign up')
+  end 
 end
